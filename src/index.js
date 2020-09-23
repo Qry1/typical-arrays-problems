41f137a2cbe8c4cd049c970c5f41f856a312237a
Stepan Sukach <stepansukac@gmail.com>
2020-09-23 22:48:36 +0300

    feat: add solution to tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
+
+  if (array !== undefined) {
+    if (array.length !== 0) {
+      return Math.min(...array);
+    };
+  };
+
   return 0;
 }
 
 exports.max = function max (array) {
+
+  if (array !== undefined) {
+    if (array.length !== 0) {
+      return Math.max(...array);
+    };
+  };
+ 
   return 0;
 }
 
 exports.avg = function avg (array) {
+
+  if (array !== undefined) {
+    if (array.length !== 0) {
+      return array.reduce((sum, current) => sum + current, 0) / array.length;
+    };
+  };
+
   return 0;
 }
+
